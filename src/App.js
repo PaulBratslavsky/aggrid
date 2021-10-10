30be4d3f25e3f7968b991090507402573ab1ae5b
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2021-10-10 15:43:42 -0500

    intro to aggrid

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-const url = "https://communityoneapi.herokuapp.com/projects";
+const url = 'https://communityoneapi.herokuapp.com/projects';
+
+function parseDataFromApi(data) {
+  return data.map((item) => {
+    const {
+      id,
+      name,
+      likes,
+      developer: { firstName, lastName },
+    } = item;
+    return {
+      id,
+      name,
+      fullName: `${firstName} ${lastName}`,
+      likes: likes.length,
+    };
+  });
+}
 
 export default function App() {
   const [rowData, setRowData] = useState([]);
+  const [isModelVisible, setIsModelVisible] = useState(true);
+  const gridRef = useRef(null);
 
   useEffect(() => {
-    async function getProjects(url, callback) {
+    async function getProjects(url) {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data)
-      const rowData = data.map(item => {
-        const { id, name, likes, developer: { firstName, lastName } } = item;
-        console.log(likes.length)
-        return {
-          id,
-          name,
-          fullName: `${firstName} ${lastName}`,
-          likes: likes.length,
-        }
-      })
-
-      setRowData(rowData)
+      const rowData = parseDataFromApi(data);
+      setRowData(rowData);
     }
 
-    getProjects(url)
-    
-  },[]);
+    getProjects(url);
+  }, []);
+
+  useEffect(() => {
+    const columnApi = gridRef.current.columnApi;
+    columnApi.setColumnVisible('id', isModelVisible);
+  }, [isModelVisible]);
 
   const columnDefs = [
     {
@@ -38,6 +50,7 @@ export default function App() {
       field: 'id',
       sortable: true,
       filter: true,
+      checkboxSelection: true,
     },
     {
       headerName: 'Project',
@@ -56,12 +69,35 @@ export default function App() {
       field: 'likes',
       sortable: true,
       filter: true,
-    }
+    },
   ];
 
+  function onButtonClick() {
+    const selectedNodes = gridRef.current.api.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map((node) => `${node.name} ${node.id}`)
+      .join(', ');
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
+
+  function toggleColumn() {
+    setIsModelVisible((prevState) => !prevState);
+  }
+
   return (
-    <div className="ag-theme-alpine" style={{ height: 400, width: 750 }}>
-      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 800 }}>
+      <button onClick={onButtonClick}>Get selected rows</button>
+      <button onClick={toggleColumn}>
+        {isModelVisible ? 'Hide Id Column' : 'Show Id Column'}
+      </button>
+
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        rowSelection="multiple"
+        columnDefs={columnDefs}
+      ></AgGridReact>
     </div>
   );
 }
